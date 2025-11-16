ff482b8fab2c27a81a2e2123ccfdea21b9f4db13
nikunjvarshney <nikunjvarshneya@gmail.com>
2025-11-16 08:54:02 +0530

    build

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}`, // /api add kar diya
+  baseURL: `${import.meta.env.VITE_API_URL}/api`, // /api add kar diya
   withCredentials: true,
 });
 
